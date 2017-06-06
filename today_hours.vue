13d441513f8f38df797391180afec498429440fc
ikvir@mobilefringe.com @ CodeCloud Staging <ikvir@mobilefringe.com @ CodeCloud Staging>
2017-06-06 12:57:58 -0400

    Single Page Application with Vue.js 2.0 1.0  /today_hours.vue

--- today_hours.vue
+++ today_hours.vue
@@ -1,75 +1,27 @@
 <template>
-  <div class="container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-    <ul class="menu">
-      <li><a v-on:click="changeMode('alphabetical')">Alphabetical</a></li>
-      <li><a v-on:click="changeMode('category')">Category</a></li>
-    </ul>
-    <hr/>
-    <div class="columns large-12" v-for="(stores, index) in storesByAlphaIndex" v-if="listMode === 'alphabetical'">
-      <div class="list_header">
-        <b>{{index}}</b>
-        <hr/>
-      </div>
-      <div class="store-section" v-for="store in stores">
-        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
-        <hr/>
-      </div>
-      <!-- <div class="card">
-        <div class="card-divider">
-          {{ store.name }}
-        </div>
-        <div class="card-section center">
-          <a :href="store.image_url" target="_blank"><img :src="store.image_url"></a>
-        </div>
-        <div class="card-section">
-          <div class="center">
-            <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">View Details</router-link>
-          </div>
-        </div>
-      </div> -->
-    </div>
-    <div class="columns large-12" v-for="(stores, index) in storesByCategoryName" v-if="listMode === 'category'">
-      <div class="list_header">
-        <b>{{index}}</b>
-        <hr/>
-      </div>
-      <div class="store-section" v-for="store in stores">
-        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>
-        <hr/>
-      </div>
-    </div>
+  <div class="container">
+    <h3>Shopping Hours</h3>
+    <p v-if="!hours.is_closed">{{todayDate | moment("ddd", timezone)}} {{hours.open_time | moment("LT", timezone)}} - {{hours.close_time | moment("LT", timezone)}}</p>
+    <p v-if="hours.is_closed">Closed</p>
   </div>
 </template>
 
-<style>
-  .center{
-    text-align: center
-  }
-  .store-section a{
-    color: #708090;
-  }
-</style>
-
 <script>
-  define(["Vue"], function(Vue) {
-    return Vue.component("stores-component", {
-      template: template, // the variable template will be injected
+  define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
+    return Vue.component("today-hours", {
+      template: template, // the variable template will be injected,
       data: function() {
         return {
-          listMode: "alphabetical"
-        }
-      },
-      methods: {
-        changeMode (mode) {
-          this.listMode = mode;
+          todayDate: new Date(),
+          description: "An example of integration of Mall Maverick with Vue.js"
         }
       },
       computed: {
-        storesByAlphaIndex() {
-          return this.$store.getters.storesByAlphaIndex;
+        hours () {
+          return this.$store.getters.getTodayHours;
         },
-        storesByCategoryName() {
-          return this.$store.getters.storesByCategoryName;
+        timezone () {
+          return this.$store.getters.getTimezone;
         }
       }
     });
