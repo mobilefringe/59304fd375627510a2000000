bc3160812c36e9d12295cd1416842a541e90ab0b
ikvir@mobilefringe.com @ CodeCloud Staging <ikvir@mobilefringe.com @ CodeCloud Staging>
2017-06-06 13:12:37 -0400

    Single Page Application with Vue.js 2.0 1.0  /default.vue

--- default.vue
+++ default.vue
@@ -5,7 +5,22 @@
 <script>
   define(["Vue"], function(Vue) {
     return Vue.component("default-component", {
-      template: template // the variable template will be injected
+      template: template, // the variable template will be injected
+        
+      beforeRouteEnter (to, from, next) {
+        next(vm => {
+          // access to component instance via `vm`
+          console.log(to);
+        })
+      },
+      beforeRouteUpdate (to, from, next) {
+        console.log(to);
+      },
+      computed: {
+        findMetaByPath () {
+          return this.$store.getters.findMetaByPath;
+        }
+      }        
     });
   });
 </script>
