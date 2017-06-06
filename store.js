264d6c9117612c311788f5124c12af2f41e0b94f
ikvir@mobilefringe.com @ CodeCloud Staging <ikvir@mobilefringe.com @ CodeCloud Staging>
2017-06-06 13:02:18 -0400

    Single Page Application with Vue.js 2.0 1.0  /store.js

--- store.js
+++ store.js
@@ -3,6 +3,7 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
   const store = new Vuex.Store({
     state: {
       results: [],
+      meta_data: [],
       locale: null
     },
     actions: {
@@ -20,11 +21,10 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
       LOAD_META_DATA: function ({ commit }) {
         return new Promise((resolve, reject) => {
           axios.get("/api/v1/get_meta_data").then(response => {
-            console.log(response);
-            //commit('SET_META_DATA', { list: response.data })
+            commit('SET_META_DATA', { list: response.data })
             resolve(response);
           }).catch(error => {
-            console.log("Data load error: " + error.message);
+            console.log("Meta Data load error: " + error.message);
             reject(error);
           });
         })
@@ -47,6 +47,9 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
       SET_LOCALE: (state, { lang }) => {
         state.locale = lang
         Cookies.set('locale', lang);
+      },
+      SET_META_DATA: (state, { list }) => {
+        state.meta_data = list
       }
     },
     getters: {
@@ -122,6 +125,10 @@ define(['Vue', 'vuex', 'axios', 'js-cookie', 'moment', 'moment-timezone', 'lodas
       findPromoBySlug: (state, getters) => (slug) => {
         let promos = getters.processedPromos;
         return promos.find(promo => promo.slug === slug)
+      },
+      findMetaDataByPath: (state, getters) => (path) => {
+        let meta_data = state.meta_data.meta_data;
+        return meta_data.filter(meta => meta.path === path)
       },
       storesByAlphaIndex: (state, getters) => {
         let stores = getters.processedStores;
